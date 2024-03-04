24602b646697ffd965fc8fee9c351a7b0aed333e
CelioSlomp <slompcelio132@gmail.com>
2024-03-04 20:24:16 -0300

    Adicionado alguns comandos sql

---
+++ src/main/java/br/edu/ifc/estoque/produtos/bd/script.sql
@@ -0,0 +1,38 @@
+create table compra(
+	idProduto integer,
+    valor double,
+    quantidade integer
+);
+
+create table valores(
+	idProduto integer,
+    valor double,
+    quantidade integer
+);
+
+CREATE TABLE cliente (
+  id int NOT NULL AUTO_INCREMENT,
+  nome varchar(255) NOT NULL,
+  bomPag int NOT NULL,
+  PRIMARY KEY (id)
+)
+
+CREATE TABLE produto (
+  id int NOT NULL AUTO_INCREMENT,
+  nome varchar(45) NOT NULL,
+  marca varchar(45) NOT NULL,
+  tipo int NOT NULL,
+  descricao varchar(255) DEFAULT NULL,
+  unidade varchar(8) NOT NULL,
+  PRIMARY KEY (id)
+)
+
+
+
+select * from produto;
+select * from cliente;
+select * from valores;
+select * from compra;
+select * from produto, valores where id=idproduto;
+delete from valores;
+delete from compra;
--- src/main/java/br/edu/ifc/estoque/produtos/controller/VendaController.java
+++ src/main/java/br/edu/ifc/estoque/produtos/controller/VendaController.java
@@ -1,14 +1,16 @@
 package br.edu.ifc.estoque.produtos.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/vendas")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class VendaController {
     @GetMapping("/listaVendas")
-    public String lista_vendas(){
+    public String lista_vendas() {
         return "lista_vendas";
     }
 }
